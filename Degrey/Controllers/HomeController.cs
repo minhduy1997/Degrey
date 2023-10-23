53a90ed6c2c6c18582049f34841256f6e2eb03dc
Nguyễn Minh Duy <duy.nm@routine.vn>
2023-10-23 09:50:25 +0700

    create new

---
+++ Degrey/Controllers/HomeController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Degrey.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Home()
+        {
+            return View();
+        }
+        public ActionResult SanPham()
+        {
+            return View();
+        }
+        public ActionResult Store()
+        {
+            return View();
+        }
+        public ActionResult GioiThieu()
+        {
+            return View();
+        }
+        public ActionResult Cart()
+        {
+            return View();
+        }
+        public ActionResult ThongTinSoHuu()
+        {
+            return View();
+        }
+        public ActionResult HuongDanDatHang()
+        {
+            return View();
+        }
+        public ActionResult ChinhSachBaoMat()
+        {
+            return View();
+        }
+        public ActionResult ChinhSachVaQuyDinh()
+        {
+            return View();
+        }
+    }
+}
